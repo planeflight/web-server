85ab3135204171f19c7243c9b79f63cf5c38b7ed
eipiequals-1 <olivier.lvannier@gmail.com>
2023-02-04 10:18:57 -0800

    Modified Readme.md, documented main.cpp

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# web-server
-Simple Web Server created in C++
+# Minimalistic Web Server in C++
+
+libGDX is licensed under MIT the MIT Licence. All uses of this software must maintain this Licence. See LICENCE for more details.
+
+## Requirements
+- GNU Make
+- C++ 20
+
+## Building
+- Clone: **git clone https://github.com/eipiequals-1/web-server**
+- Build: **make** or **make -j 8** depending on the number of cores available.
+- Run: **./bin/web_server**
+
+## Getting Started
+- View web-server/main.cpp for a workflow overview!
--- main.cpp
+++ main.cpp
@@ -5,25 +5,34 @@
 #include <iostream>
 
 auto main() -> int {
+    // create new htpp::Server() on port 8080
     http::Server *app = new http::Server(8080);
 
+    // set html, static files path for resources
     http::response::set_html_path("public/html/");
     http::response::set_static_path("public/static/");
 
+    // create a route at /index
     app->add_route("/index", [](const std::string &request) -> std::string {
+        // check if the request is a get request
         if (request == http::request::get) {
+            // return the contents of the index.html file
             return http::response::read_file("index.html");
         }
         return "FAILURE";
     });
 
+    // create a route at /about
     app->add_route("/about", [](const std::string &request) -> std::string {
+        // check if it is a get request
         if (request == http::request::get) {
+            // return the contents of the about.html file
             return http::response::read_file("about.html");
         }
         return "FAILURE";
     });
 
+    // run and delete the app
     app->run();
     delete app;
 
